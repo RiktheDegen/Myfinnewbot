32a57111e552a765a6c6da88e19389d02a1f4e9d
Srinath Hariharan <shacstudios@gmail.com>
2024-01-06 16:23:51 -0500

    make it better

--- server.mjs
+++ server.mjs
@@ -5,6 +5,13 @@ import path from 'path';
 const app = express();
 const port = process.env.PORT || 3000;
 
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'dist')));
 
