ae8c4ea7864842582b286224c851de4c07caf71b
Srinath Hariharan <shacstudios@gmail.com>
2024-01-06 16:29:36 -0500

    make it better

--- server.mjs
+++ server.mjs
@@ -1,9 +1,11 @@
+import { createServer } from 'http';
 import express  from 'express';
 import path from 'path';
 
 
 const app = express();
 const port = process.env.PORT || 3000;
+const server = createServer(app);
 
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
@@ -13,8 +15,8 @@ const __dirname = dirname(__filename);
 
 
 // Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'dist/assets')));
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
